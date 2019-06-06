3f5bfb914e66ba30e33845d49e4b398a021e9e19
ZacLinz <zaclinzmeyer@gmail.com>
2019-06-06 11:07:17 -0600

    attempt to add user to store

--- src/components/movie-view/movie-view.jsx
+++ src/components/movie-view/movie-view.jsx
@@ -1,37 +1,62 @@
 import React from "react";
-import { connect } from 'react-redux';
+import { connect } from "react-redux";
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 //import Card from 'react-bootstrap/Card';
-//import Button from "react-bootstrap/Button";
+import Button from "react-bootstrap/Button";
 //import Form from 'react-bootstrap/Form';
-//import { Link } from "react-router-dom";
+import { Link } from "react-router-dom";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 
 function MovieView(props) {
-  const { movies, movieId } = props
+  const { movies, movieId } = props;
 
-    if (!movies || !movies.length) return null;
-                                    //should just be double = not triple
-    const movie = movies.find(m => m._id == movieId)
+  if (!movies || !movies.length) return null;
+  //should just be double = not triple
+  const movie = movies.find(m => m._id == movieId);
 
-    return (
-      <Container className="movie-view">
-        <img className="movie-poster" src={movie.ImagePath} />
-        <Row className="movie-title">
-          <Col lg="2" className="label">
-            Title:{" "}
-          </Col>
-          <Col className="value">{movie.title}</Col>
-        </Row>
-        <Row className="movie-description">
-          <Col lg="2" className="label">
-            Description:{" "}
-          </Col>
-          <Col className="value">{movie.description}</Col>
-        </Row>
+  return (
+    <Router>
+    <Container className="movie-view">
+      <img className="movie-poster" src={movie.ImagePath} />
+      <Row className="movie-title">
+        <Col lg="2" className="label">
+          Title:{" "}
+        </Col>
+        <Col className="value">{movie.title}</Col>
+      </Row>
+      <Row className="movie-description">
+        <Col lg="2" className="label">
+          Description:{" "}
+        </Col>
+        <Col className="value">{movie.description}</Col>
+      </Row>
 
-      </Container>
-    );
-  }
-export default connect(({movies}) => ({movies}))(MovieView);
+      <Row className="movie-genre">
+        <Col lg="2" className="label">
+          Genre:{" "}
+        </Col>
+        <Col className="value">{movie.genre.name}</Col>
+        <Link to={`/genres/${movie.genre.name}`}>
+          <Button className="submit" variant="link">
+            Click here
+          </Button>
+        </Link>
+      </Row>
+      <Row className="movie-director">
+        <Col lg="2" className="label">
+          Director:{" "}
+        </Col>
+        <Col className="value">{movie.director.name}</Col>
+        <Link to={`/directors/${movie.director.name}`}>
+          <Button className="submit" variant="link">
+            Click here
+          </Button>
+        </Link>
+      </Row>
+    </Container>
+    </Router>
+  );
+}
+export default connect(({ movies }) => ({ movies }))(MovieView);
--- src/reducers/reducers.js
+++ src/reducers/reducers.js
@@ -1,6 +1,6 @@
 import { combineReducers } from 'redux';
 
-import { SET_FILTER, SET_SORT_COLUMN, SET_MOVIES } from '../actions/actions';
+import { SET_FILTER, SET_SORT_COLUMN, SET_MOVIES, SET_USER } from '../actions/actions';
 
 function visibilityFilter( state = '', action){
   switch (action.type){
@@ -29,10 +29,20 @@ function movies(state = [], action){
   }
 }
 
+function user(state = [], action){
+  switch (action.type){
+    case SET_USER:
+      return action.value;
+    default:
+    return state;
+  }
+}
+
 const moviesApp = combineReducers({
   visibilityFilter,
   sortColumn,
-  movies
+  movies,
+  user
 })
 
 export default moviesApp;
